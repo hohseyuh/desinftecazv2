1c8177e506b4f8f1644aea9b793c2b1cd37609c1
Hüseyn Moylayev <126387535+hohseyuh@users.noreply.github.com>
2026-04-10 18:54:56 +0400

    Imago Mundi

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: '/kur',
+        destination: 'https://drive.google.com/drive/folders/17TfpskOvoC72RQOdNqErDNqDaUDxMuw8',
+        permanent: false,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
